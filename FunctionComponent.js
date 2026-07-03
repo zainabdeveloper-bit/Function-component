0e5471f08d57e50eaf6711a90929fb49856ab6ce
Zainab Shehzadi <zainabzani198@gmail.com>
2026-07-03 11:43:02 -0700

    Add files via upload

---
+++ FunctionComponent.js
@@ -0,0 +1,11 @@
+function FunctionComponent(){
+   return(
+    <div>
+    <h1>Hello World</h1>
+    <h2>Second heading</h2>
+    <h3>Third heading</h3>
+    </div>
+    
+   )
+}
+export default FunctionComponent;
